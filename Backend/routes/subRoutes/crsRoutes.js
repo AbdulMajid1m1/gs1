a56c72ac7517a467f177fce2bdd653c5005db50f
unknown <abdulmajid1m2@gmail.com>
2023-11-29 17:58:59 +0500

    added vite frontends

--- Backend/routes/subRoutes/crsRoutes.js
+++ Backend/routes/subRoutes/crsRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createCrs, getAllCrs, getCrsById, updateCrs, deleteCrs } from '../../controllers/crsContoller.js';
+import { createCrs, getAllCrs, getCrsById, updateCrs, deleteCrs, getCrsByKeyword } from '../../controllers/crsContoller.js';
 
 
 
@@ -8,7 +8,8 @@ const router = express.Router();
 // Routes
 router.post('/', createCrs); // Create a new CR
 router.get('/', getAllCrs); // Get all CRs
-router.get('/:id', getCrsById); // Get a CR by ID
+router.get('/getCrsById/:id', getCrsById); // Get a CR by ID
+router.get('/seachByKeyword', getCrsByKeyword); // Get a CR by ID
 router.put('/:id', updateCrs); // Update a CR by ID
 router.delete('/:id', deleteCrs); // Delete a CR by ID
 
