bb661c446a3e53005b204b494edadcea703dc325
Eissanoor <86971746+Eissanoor@users.noreply.github.com>
2023-12-14 22:45:57 +0500

    unit popup

---
+++ Frontend/src/Pages/AdminPages/MasterData/Units/addunit.jsx
@@ -0,0 +1,121 @@
+import React, { useState } from 'react'
+import { toast } from 'react-toastify';
+import newRequest from '../../../../utils/userRequest';
+
+const Addunit = ({ isVisible, setVisibility, refreshBrandData }) => {
+    const [unit_code, setunit_code] = useState("");
+    const [unit_name, setunit_name] = useState("");
+    
+    const handleCloseCreatePopup = () => {
+        setVisibility(false);
+      };
+    
+
+    const handleAddCompany = async () => {
+    //  integrate the post api in try catch blcck
+    try {
+      const response = await newRequest.post('/units/', {
+          unit_code: unit_code,
+          unit_name: unit_name,
+        status: 1,
+      });
+
+      toast.success(`unit_code ${unit_code} has been added successfully.`, {
+        position: 'top-right',
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: 'light',
+      });
+
+
+      console.log(response.data);
+      refreshBrandData();
+      handleCloseCreatePopup();
+
+
+    } catch (error) {
+      toast.error(error?.response?.data?.error || 'Error', {
+        position: 'top-right',
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: 'light',
+      });
+
+
+      console.log(error);
+    }
+
+
+  };
+
+   
+  return (
+    <div>
+          {/* create the post api popup */}
+          {isVisible && (
+                    <div className="popup-overlay">
+                      <div className="popup-container h-auto sm:w-[45%] w-full">
+                        <div className="popup-form w-full">         
+                           <form className='w-full'>
+                             <h2 className='text-secondary font-sans font-semibold text-2xl'>Add Unit</h2>
+                             <div className="flex flex-col sm:gap-3 gap-3 mt-5">
+                               <div className="w-full font-body sm:text-base text-sm flex flex-col gap-2">
+                                 <label htmlFor="field1" className="text-secondary">Unit code</label>
+                                 <input
+                                   type="text"
+                                   id="unit_code"
+                                   value={unit_code}
+                                   onChange={(e) => setunit_code(e.target.value)}
+                                   placeholder="Enter Unit code"
+                                   className="border-1 w-full rounded-sm border-[#8E9CAB] p-2 mb-3"
+                                 />
+                               </div>
+
+                               <div className="w-full font-body sm:text-base text-sm flex flex-col gap-2">
+                                 <label htmlFor="field1" className="text-secondary">Unit name</label>
+                                 <input
+                                   type="text"
+                                   id="unit_name"
+                                   value={unit_name}
+                                   onChange={(e) => setunit_name(e.target.value)}
+                                   placeholder="Enter Unit name"
+                                   className="border-1 w-full rounded-sm border-[#8E9CAB] p-2 mb-3"
+                                 />
+                               </div>
+                             </div>
+
+                             <div className="w-full flex justify-center items-center gap-8 mt-5">
+                               <button
+                                 type="button"
+                                 className="px-5 py-2 w-[30%] rounded-sm bg-primary text-white font-body text-sm"
+                                 onClick={handleCloseCreatePopup}
+                               >
+                                 Close
+                               </button>
+                               <button
+                                 type="button"
+                                 onClick={handleAddCompany}
+                                 className="px-5 py-2 rounded-sm w-[70%] bg-secondary text-white font-body text-sm ml-2"
+                               >
+                                 Add Unit
+                               </button>
+                             </div>
+                           </form>
+                         </div>
+                       </div>
+                     </div>
+                   )}
+                    
+    </div>
+  )
+}
+
+export default Addunit
