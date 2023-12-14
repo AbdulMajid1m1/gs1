f5de971e90e3cb1707c868a469c2906a69d4babe
Eissanoor <86971746+Eissanoor@users.noreply.github.com>
2023-12-14 19:04:59 +0500

    country popup

--- Frontend/src/Pages/AdminPages/MasterData/country/country.jsx
+++ Frontend/src/Pages/AdminPages/MasterData/country/country.jsx
@@ -11,7 +11,7 @@ import newRequest from '../../../../utils/userRequest'
 import { useQuery } from 'react-query'
 import Swal from 'sweetalert2';
 import {toast} from 'react-toastify';
-
+import Addcountry from './addcounrty';
 const Country = () => {
 
     const [isLoading, setIsLoading] = useState(true);
@@ -21,7 +21,11 @@ const Country = () => {
     const { rowSelectionModel, setRowSelectionModel,
       tableSelectedRows, setTableSelectedRows } = useContext(DataTableContext);
     const [filteredData, setFilteredData] = useState([]);
+const [isCreatePopupVisible, setCreatePopupVisibility] = useState(false);
 
+    const handleShowCreatePopup = () => {
+      setCreatePopupVisibility(true);
+    };
       useEffect(() => {
       const fetchData = async () => {
         try {
@@ -40,92 +44,23 @@ const Country = () => {
       fetchData(); // Calling the function within useEffect, not inside itself
     }, []); // Empty array dependency ensures this useEffect runs once on component mount
 
-    // const { isLoading, error, data, isFetching } = useQuery("fetchPaymentSlip", async () => {
-    //   const response = await newRequest.get("/bankslip",);
-    //   return response?.data || [];
-    //   console.log(response.data);
-      
-    // });
+     const refreshcitiesData = async () => {
+      try {
+        const response = await newRequest.get("/address/getAllCountries",);
+        
+        console.log(response.data);
+        setData(response?.data || []);
+        setIsLoading(false)
+
+      } catch (err) {
+        console.log(err);
+        setIsLoading(false)
+      }
+    };
 
     const handleView = (row) => {
         console.log(row);
     }
-const handleAddCompany = async () => {
-      const { value: formValues } = await Swal.fire({
-        title: 'Create Country',
-        html:
-          '<input id="name_en" class="swal2-input" placeholder="name english">' +
-              '<input id="name_ar" class="swal2-input" placeholder="name arabic">' +
-          '<input id="country_code" class="swal2-input" placeholder="country code">' +
-          '<input id="country_shortName" class="swal2-input" placeholder="country short Name">',
-          showCancelButton: true,
-          focusConfirm: false,
-          confirmButtonText: '<i class="fa fa-thumbs-up"></i> Create Country',
-          confirmButtonAriaLabel: 'Create',
-          cancelButtonText: '<i class="fa fa-thumbs-down"></i> Cancel',
-          cancelButtonAriaLabel: 'Cancel',  
-          confirmButtonColor: '#021F69',
-
-        preConfirm: () => {
-          return {
-            name_en: document.getElementById('name_en').value,
-              name_ar: document.getElementById('name_ar').value,
-            country_code: document.getElementById('country_code').value,
-            country_shortName: document.getElementById('country_shortName').value,
-          };
-        },
-        inputValidator: (form) => {
-          if (!form.name_en  || !form.name_ar || !form.country_code || !form.country_shortName  ) {
-            return 'All Input field is required';
-          }
-        },
-      });
-  
-      if (!formValues) {
-        return; // Cancelled or invalid input
-      }
-  
-      const { name_en, name_ar,country_code,country_shortName } = formValues;
-  
-      try {
-        // Send a request to your API to add the company
-        const response = await newRequest.post('/address/createCountries/', {
-          name_en: name_en,
-            name_ar: name_ar,
-          country_code: country_code,
-            country_shortName: country_shortName,
-          status:1
-        });
-  
-        toast.success(`name_en ${name_en} with name_ar "${name_ar}" has been added successfully.`, {
-          position: "top-right",
-          autoClose: 2000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-          theme: "light",
-
-        });
-
-        console.log(response.data);
-  
-      } catch (error) {
-        toast.error(error?.response?.data?.error || 'Error', {
-          position: "top-right",
-          autoClose: 2000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-          theme: "light",
-        });
-
-        console.log(error);
-      }
-    };
     const handleRowClickInParent = (item) => {
         if (!item || item?.length === 0) {
           setTableSelectedRows(data)
@@ -172,7 +107,7 @@ const handleAddCompany = async () => {
                         {/* </div> */}
 <div className='flex justify-start sm:justify-start items-center flex-wrap gap-2 py-7 px-3'>
                         <button
-                          onClick={handleAddCompany}
+                          onClick={handleShowCreatePopup}
                             className="rounded-full bg-secondary font-body px-5 py-1 text-sm mb-3 text-white transition duration-200 hover:bg-primary">
                               <i className="fas fa-plus mr-2"></i>Add
                         </button>
@@ -209,7 +144,10 @@ const handleAddCompany = async () => {
                 </div>
               </div>
             </div>
-      
+       {/* Addcountry component with handleShowCreatePopup prop */}
+             {isCreatePopupVisible && (
+                    <Addcountry isVisible={isCreatePopupVisible} setVisibility={setCreatePopupVisibility} refreshBrandData={refreshcitiesData}/>
+                  )}
 
 
         </div>
