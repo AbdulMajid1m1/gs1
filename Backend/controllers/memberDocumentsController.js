e4a7765230f46ebead6e1ccd6e93a16382ea9579
unknown <abdulmajid1m2@gmail.com>
2023-12-18 11:23:31 +0500

    added member_documents apis

---
+++ Backend/controllers/memberDocumentsController.js
@@ -0,0 +1,175 @@
+import prisma from '../prismaClient.js';
+import Joi from 'joi';
+import { createError } from '../utils/createError.js';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+export const createMemberDocument = async (req, res, next) => {
+    // Validate body data
+    const schema = Joi.object({
+        type: Joi.string().required(),
+        transaction_id: Joi.string().allow(''),
+        user_id: Joi.string().required(),
+        admin_id: Joi.string().allow(''),
+        doc_type: Joi.string().default('member_document')
+    });
+
+    const { error, value } = schema.validate(req.body);
+    if (error) {
+        return next(createError(400, error.details[0].message));
+    }
+
+    // Get uploaded document
+    const uploadedDocument = req.files.document;
+    if (!uploadedDocument) {
+        return next(createError(400, 'Document is required'));
+    }
+
+    const documentFile = uploadedDocument[0];
+    const documentName = documentFile.filename;
+    documentFile.destination = documentFile.destination.replace('public', '');
+    const documentPath = path.join(documentFile.destination, documentName);
+
+    // Save document details in the database using Prisma
+    try {
+        const newDocument = await prisma.member_documents.create({
+            data: {
+                type: value.type,
+                document: documentPath,
+                transaction_id: value.transaction_id,
+                user_id: value.user_id,
+                admin_id: value.admin_id || "",
+                doc_type: value.doc_type
+            }
+        });
+
+        res.status(201).json({
+            message: 'Document uploaded successfully.',
+            memberDocument: newDocument,
+        });
+    } catch (err) {
+        console.log(err);
+        return next(err);
+    }
+};
+
+
+const allowedColumns = {
+    id: Joi.string(),
+    type: Joi.string(),
+    transaction_id: Joi.string(),
+    // ... other fields
+};
+
+const filterSchema = Joi.object(
+    Object.keys(allowedColumns).reduce((schema, column) => {
+        schema[column] = allowedColumns[column];
+        return schema;
+    }, {})
+).unknown(false);
+
+export const getMemberDocuments = async (req, res, next) => {
+    try {
+        const { error, value } = filterSchema.validate(req.query);
+        if (error) {
+            return next(createError(400, `Invalid query parameter: ${error.details[0].message}`));
+        }
+
+        const filterConditions = Object.keys(value).length > 0
+            ? Object.keys(value).reduce((obj, key) => {
+                obj[key] = value[key];
+                return obj;
+            }, {})
+            : {};
+
+        const documents = await prisma.member_documents.findMany({ where: filterConditions });
+        res.json(documents);
+    } catch (error) {
+        next(error);
+    }
+};
+
+
+const updateMemberDocumentSchema = Joi.object({
+    type: Joi.string().max(255), // Assuming a string with max length 255
+    document: Joi.string(),      // This will be the path of the document
+    transaction_id: Joi.string().max(255).allow(''), // Allowing empty string
+    user_id: Joi.string(),   // Assuming user_id is a positive integer
+    admin_id: Joi.string(),  // Assuming admin_id is a positive integer
+    doc_type: Joi.string().max(20).default('member_document'), // Assuming max length 20 and default value
+
+
+});
+
+
+export const updateMemberDocument = async (req, res, next) => {
+    const documentId = req.params.id;
+    if (!documentId) {
+        return next(createError(400, 'Document ID is required'));
+    }
+
+    // Validate the request body
+    const { error, value } = updateMemberDocumentSchema.validate(req.body);
+    if (error) {
+        return next(createError(400, error.details[0].message));
+    }
+
+    try {
+        // Retrieve the current document from the database
+        const currentDocument = await prisma.member_documents.findUnique({
+            where: { id: documentId }
+        });
+
+        if (!currentDocument) {
+            return next(createError(404, 'Document not found'));
+        }
+
+        // Check for a new document upload
+        if (req.files && req.files.document) {
+            const documentFile = req.files.document[0];
+            const newDocumentPath = path.join(documentFile.destination, documentFile.filename);
+
+            // Calculate the directory of the current module
+            const dirname = path.dirname(fileURLToPath(import.meta.url));
+            const oldDocumentPath = path.join(dirname, '..', currentDocument.document);
+
+            if (fs.existsSync(oldDocumentPath)) {
+                fs.unlinkSync(oldDocumentPath);
+            }
+
+            // Update the document path in the value to be saved
+            value.document = newDocumentPath;
+        }
+
+        // Update the document in the database
+        const updatedDocument = await prisma.member_documents.update({
+            where: { id: documentId },
+            data: value
+        });
+
+        res.json(updatedDocument);
+    } catch (err) {
+        next(err);
+    }
+};
+
+
+export const deleteMemberDocument = async (req, res, next) => {
+    const schema = Joi.object({
+        id: Joi.string().required(),
+    });
+    const { error, value } = schema.validate(req.params);
+    if (error) {
+        return next(createError(400, error.details[0].message));
+    }
+
+    const documentId = value.id;
+
+    try {
+        await prisma.member_documents.delete({ where: { id: documentId } });
+        res.status(204).send();
+    } catch (err) {
+        next(err);
+    }
+};
