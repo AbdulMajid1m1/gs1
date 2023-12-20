69aacf26eb54be2b8b4c09d8d30e5dd6db887461
Eissanoor <86971746+Eissanoor@users.noreply.github.com>
2023-12-20 14:08:43 +0500

    brands

--- Frontend/src/Pages/AdminPages/AdminIndentify/Brands/Brands.jsx
+++ Frontend/src/Pages/AdminPages/AdminIndentify/Brands/Brands.jsx
@@ -10,15 +10,27 @@ import DeleteIcon from "@mui/icons-material/Delete";
 import newRequest from '../../../../utils/userRequest';
 import Swal from 'sweetalert2';
 import { toast } from 'react-toastify';
-
+import Addbrands from './addbrands';
+import Updatebrands from './updatebrands';
 const Brands = () => {
     const [isLoading, setIsLoading] = useState(false);
     const [data, setData] = useState([]);
     const gs1MemberData = JSON.parse(sessionStorage.getItem("gs1memberRecord"));
     console.log(gs1MemberData)
-   
+   const [isCreatePopupVisible, setCreatePopupVisibility] = useState(false);
+
+    const handleShowCreatePopup = () => {
+      setCreatePopupVisibility(true);
+    };
     const navigate = useNavigate();
-    
+    const [isUpdatePopupVisible, setUpdatePopupVisibility] = useState(false);
+
+      const handleShowUpdatePopup = (row) => {
+        setUpdatePopupVisibility(true);
+        // console.log(row)
+        // save this row data in session storage 
+        sessionStorage.setItem("updateBrandData", JSON.stringify(row));
+      };
     const { rowSelectionModel, setRowSelectionModel,
       tableSelectedRows, setTableSelectedRows } = useContext(DataTableContext);
     const [filteredData, setFilteredData] = useState([]);
@@ -26,7 +38,7 @@ const Brands = () => {
       useEffect(() => {
       const fetchData = async () => {
         try {
-          const response = await newRequest.get(`/brands?user_id=${gs1MemberData?.id}`,);
+          const response = await newRequest.get(`/brands`,);
           
           console.log(response.data);
           setData(response?.data || []);
@@ -42,9 +54,9 @@ const Brands = () => {
     }, []); // Empty array dependency ensures this useEffect runs once on component mount
 
     // refreshData
-    const refreshData = async () => {
+     const refreshcitiesData = async () => {
       try {
-        const response = await newRequest.get(`/brands?user_id=${gs1MemberData?.id}`,);
+        const response = await newRequest.get("/brands",);
         
         console.log(response.data);
         setData(response?.data || []);
@@ -55,15 +67,79 @@ const Brands = () => {
         setIsLoading(false)
       }
     };
-
-
+const handleView = (row) => {
+        console.log(row);
+    }
+const handleDelete = async (row) => {
+        Swal.fire({
+          title: 'Are you sure?',
+          text: 'You will not be able to recover this cr number!',
+          icon: 'warning',
+          showCancelButton: true,
+          confirmButtonText: 'Yes, delete it!',
+          cancelButtonText: 'No, keep it',
+          // changes the color of the confirm button to red
+          confirmButtonColor: '#1E3B8B',
+          cancelButtonColor: '#FF0032',
+        }).then(async (result) => {
+          if (result.isConfirmed) {
+            try {
+              const isDeleted = await newRequest.delete("/brands/" + row?.id);
+              if (isDeleted) {
+                toast.success('Cr number deleted successfully', {
+                  position: "top-right",
+                  autoClose: 2000,
+                  hideProgressBar: false,
+                  closeOnClick: true,
+                  pauseOnHover: true,
+                  draggable: true,
+                  progress: undefined,
+                  theme: "light",
+                });
+  
+                
+                // filter out the deleted user from the data
+                const filteredData = brandsData.filter((item) => item?.id !== row?.id);
+                setBrandsData(filteredData);
+                
+              } else {
+                // Handle any additional logic if the user was not deleted successfully
+                toast.error('Failed to delete user', {
+                  position: "top-right",
+                  autoClose: 2000,
+                  hideProgressBar: false,
+                  closeOnClick: true,
+                  pauseOnHover: true,
+                  draggable: true,
+                  progress: undefined,
+                  theme: "light",
+                });
+  
+              }
+            } catch (error) {
+              // Handle any error that occurred during the deletion
+              console.error("Error deleting user:", error);
+              toast.error('Something went wrong while deleting user', {
+                position: "top-right",
+                autoClose: 2000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "light",
+              });
+            }
+          } else if (result.dismiss === Swal.DismissReason.cancel) {
+            return;
+          }
+        });
+    };
     const handleEdit = (row) => {
       console.log(row);
     }
 
-    const handleDelete = (row) => {
-      console.log(row);
-    }
+   
 
   const handleRowClickInParent = (item) => {
       if (!item || item?.length === 0) {
@@ -164,7 +240,7 @@ const Brands = () => {
 
                     <div className='flex justify-start sm:justify-start items-center flex-wrap gap-2 py-7 px-3'>
                         <button
-                          onClick={handleAddCompany}
+                          onClick={handleShowCreatePopup}
                             className="rounded-full bg-secondary font-body px-5 py-1 text-sm mb-3 text-white transition duration-200 hover:bg-primary">
                               <i className="fas fa-plus mr-2"></i>Add
                         </button>
@@ -180,29 +256,41 @@ const Brands = () => {
                         handleRowClickInParent={handleRowClickInParent}
 
                         dropDownOptions={[
-                            {
-                            label: "Edit",
-                            icon: (
-                              <EditIcon
-                                fontSize="small"
-                                color="action"
-                                style={{ color: "rgb(37 99 235)" }}
-                              />
-                            ),
-                            action: handleEdit,
-                          },
-                          {
-                            label: "Delete",
-                            icon: (
-                              <DeleteIcon
-                                fontSize="small"
-                                color="action"
-                                style={{ color: "rgb(37 99 235)" }}
-                              />
-                            ),
-                            action: handleDelete,
-                          },
-                        ]}
+                        {
+                        label: "View",
+                        icon: (
+                            <VisibilityIcon
+                            fontSize="small"
+                            color="action"
+                            style={{ color: "rgb(37 99 235)" }}
+                            />
+                        ),
+                        action: handleView,
+                        },
+{
+                                    label: "Edit",
+                                    icon: (
+                                        <EditIcon
+                                        fontSize="small"
+                                        color="action"
+                                        style={{ color: "rgb(37 99 235)" }}
+                                        />
+                                    ),
+                                    action: handleShowUpdatePopup,
+                                },
+                                {
+                                    label: "Delete",
+                                    icon: (
+                                        <DeleteIcon
+                                        fontSize="small"
+                                        color="action"
+                                        style={{ color: "rgb(37 99 235)" }}
+                                        />
+                                    ),
+                                    action: handleDelete,
+                                },
+
+                    ]}
                         uniqueId="gtinMainTableId"
                       />
                       </div>
@@ -210,7 +298,15 @@ const Brands = () => {
 
                   </div>
                 </div>
-              </div>
+        </div>
+        {/* Addbrands component with handleShowCreatePopup prop */}
+             {isCreatePopupVisible && (
+                    <Addbrands isVisible={isCreatePopupVisible} setVisibility={setCreatePopupVisibility} refreshBrandData={refreshcitiesData}/>
+        )}
+        {/* Updatebrands component with handleShowUpdatePopup prop */}
+                  {isUpdatePopupVisible && (
+                    <Updatebrands isVisible={isUpdatePopupVisible} setVisibility={setUpdatePopupVisibility} refreshBrandData={refreshcitiesData}/>
+                  )}
         </div>
     </div>
   )
