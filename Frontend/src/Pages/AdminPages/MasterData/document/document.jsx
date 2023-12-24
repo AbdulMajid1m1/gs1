c39083b2ce269b0a4d491e767ba2178cf985d1d4
Eissanoor <86971746+Eissanoor@users.noreply.github.com>
2023-12-24 12:05:58 +0500

    cleaning code

--- Frontend/src/Pages/AdminPages/MasterData/document/document.jsx
+++ Frontend/src/Pages/AdminPages/MasterData/document/document.jsx
@@ -1,6 +1,4 @@
 import React, { useContext, useEffect, useState } from 'react'
-// import visitFrontend from "../../../Images/visitFrontend.png"
-// import profileICon from "../../../Images/profileICon.png"
 import DataTable from '../../../../components/Datatable/Datatable'
 import { useNavigate } from 'react-router-dom'
 import VisibilityIcon from "@mui/icons-material/Visibility";
@@ -29,8 +27,7 @@ const [brandsData, setBrandsData] = useState([]);
 
       const handleShowUpdatePopup = (row) => {
         setUpdatePopupVisibility(true);
-        // console.log(row)
-        // save this row data in session storage 
+    
         sessionStorage.setItem("updateBrandData", JSON.stringify(row));
       };
     const { rowSelectionModel, setRowSelectionModel,
@@ -52,15 +49,10 @@ const [brandsData, setBrandsData] = useState([]);
         }
       };
 
-      fetchData(); // Calling the function within useEffect, not inside itself
-    }, []); // Empty array dependency ensures this useEffect runs once on component mount
+      fetchData(); 
+    }, []); 
 
-    // const { isLoading, error, data, isFetching } = useQuery("fetchPaymentSlip", async () => {
-    //   const response = await newRequest.get("/bankslip",);
-    //   return response?.data || [];
-    //   console.log(response.data);
-      
-    // });
+   
  const refreshcitiesData = async () => {
       try {
         const response = await newRequest.get("/getAllcr_documents",);
@@ -85,7 +77,7 @@ const handleDelete = async (row) => {
           showCancelButton: true,
           confirmButtonText: 'Yes, delete it!',
           cancelButtonText: 'No, keep it',
-          // changes the color of the confirm button to red
+       
           confirmButtonColor: '#1E3B8B',
           cancelButtonColor: '#FF0032',
         }).then(async (result) => {
@@ -103,14 +95,12 @@ const handleDelete = async (row) => {
                   progress: undefined,
                   theme: "light",
                 });
-  
-                
-                // filter out the deleted user from the data
+
                 const filteredData = brandsData.filter((item) => item?.id !== row?.id);
                 setBrandsData(filteredData);
                 refreshcitiesData()
               } else {
-                // Handle any additional logic if the user was not deleted successfully
+              
                 toast.error('Failed to delete user', {
                   position: "top-right",
                   autoClose: 2000,
@@ -124,7 +114,7 @@ const handleDelete = async (row) => {
   
               }
             } catch (error) {
-              // Handle any error that occurred during the deletion
+             
               console.error("Error deleting user:", error);
               toast.error('Something went wrong while deleting user', {
                 position: "top-right",
@@ -164,36 +154,20 @@ const handleDelete = async (row) => {
               <div className="h-auto w-[97%] px-0 pt-4">
                 <div className="h-auto w-full p-0 bg-white shadow-xl rounded-md">
 
-                    {/* Buttons */}
-                    {/* <div className='h-auto w-full shadow-xl'> */}
-                        {/* <div className='flex justify-center sm:justify-start items-center flex-wrap gap-2 py-3 px-3'>
-                            <button
-                              onClick={() => navigate('/member/bank-slip')}
-                                className="rounded-full bg-primary font-body px-5 py-1 text-sm mb-3 text-white transition duration-200 hover:bg-secondary active:bg-blue-700">
-                                 <i className="fas fa-plus mr-1"></i>Update Documents
-                            </button>
-
-                            <button
-                            className="rounded-full bg-[#1E3B8B] font-body px-5 py-1 text-sm mb-3 text-white transition duration-200 hover:bg-primary active:bg-blue-700">
-                                 Pendings <i className="fas fa-caret-down ml-1"></i>
-                            </button>
-
-                            <button
-                            className="rounded-full bg-[#1E3B8B] font-body px-5 py-1 text-sm mb-3 text-white transition duration-200 hover:bg-primary active:bg-blue-700"
-                            // onClick={handleExportProducts}
-                            >
-                                 Rejected <i className="fas fa-caret-down ml-1"></i>
-                            </button>
-                          </div> */}
-                        {/* </div> */}
+                  
   <div className='flex justify-start sm:justify-start items-center flex-wrap gap-2 py-7 px-3'>
                         <button
                           onClick={handleShowCreatePopup}
                             className="rounded-full bg-secondary font-body px-5 py-1 text-sm mb-3 text-white transition duration-200 hover:bg-primary">
                               <i className="fas fa-plus mr-2"></i>Add
+                </button>
+                  <button
+                          onClick={handleShowCreatePopup}
+                            className="rounded-full bg-secondary font-body px-5 py-1 text-sm mb-3 text-white transition duration-200 hover:bg-primary">
+                              <i className="fas fa-plus mr-2"></i>Import
                         </button>
                     </div>
-                    {/* DataGrid */}
+                   
                     <div style={{ marginLeft: '-11px', marginRight: '-11px' }}>
 
                     <DataTable data={data} 
@@ -247,13 +221,9 @@ const handleDelete = async (row) => {
                 </div>
               </div>
             </div>
-      
-
-             {/* Adddocumment component with handleShowCreatePopup prop */}
              {isCreatePopupVisible && (
                     <Adddocumment isVisible={isCreatePopupVisible} setVisibility={setCreatePopupVisibility} refreshBrandData={refreshcitiesData}/>
                   )}
-{/* Updatedocument component with handleShowUpdatePopup prop */}
                   {isUpdatePopupVisible && (
                     <Updatedocument isVisible={isUpdatePopupVisible} setVisibility={setUpdatePopupVisibility} refreshBrandData={refreshcitiesData}/>
                   )}
