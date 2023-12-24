4084b8ed5601c524ab72913575516d0946b2942a
Eissanoor <86971746+Eissanoor@users.noreply.github.com>
2023-12-24 22:46:47 +0500

    cities import

--- Frontend/src/Pages/AdminPages/MasterData/city/cities.jsx
+++ Frontend/src/Pages/AdminPages/MasterData/city/cities.jsx
@@ -10,7 +10,9 @@ import Swal from 'sweetalert2';
 import {toast} from 'react-toastify';
 import AddCity from './AddCity';
 import Updatecity from './updatecity';
-
+import * as XLSX from 'xlsx';
+import { CSVLink } from "react-csv";
+import FileUploadIcon from '@mui/icons-material/FileUpload';
 const Cities = () => {
 
     const [isLoading, setIsLoading] = useState(true);
@@ -127,7 +129,7 @@ const refreshcitiesData = async () => {
                 // filter out the deleted user from the data
                 const filteredData = brandsData.filter((item) => item?.id !== row?.id);
                 setBrandsData(filteredData);
-                refreshcitiesData()
+               fetchData()
               } else {
                 // Handle any additional logic if the user was not deleted successfully
                 toast.error('Failed to delete user', {
@@ -175,7 +177,47 @@ const refreshcitiesData = async () => {
   
     }
 
+const handleFileUpload = (e) => {
+        const file = e.target.files[0];
+        if (file) {
+            console.log(file.type);
+            const reader = new FileReader();
+            reader.onload = (e) => {
+                const data = new Uint8Array(e.target.result);
+                const workbook = XLSX.read(data, { type: 'array' });
+                const sheetName = workbook.SheetNames[0]; // Assuming you have data in the first sheet
+                const sheet = workbook.Sheets[sheetName];
+                const json = XLSX.utils.sheet_to_json(sheet);
+                json.forEach((item) => {
+                    newRequest.post(`/address/createCities`, {
+                        name: item.name, // Adjust property names as needed
+                        state_id:0
+                    })
+                        .then((res) => {
+                            console.log('Add', res.data);
+                            
+                            Swal.fire(
+                                'Add!',
+                                `City has been created`,
+                                'success'
+                            )
+                           fetchData()
+                        })
+                        .catch((err) => {
+                            console.log(err);
+                            Swal.fire(
+                                'Error!',
+                                `Some City code already exist`,
+                                'error'
+                            )
+                            // Handle errors
+                        });
+                });
+            };
+            reader.readAsArrayBuffer(file);
 
+        }
+    };
 
   return (
     <div>
@@ -195,7 +237,26 @@ const refreshcitiesData = async () => {
                           onClick={handleShowCreatePopup}
                             className="rounded-full bg-secondary font-body px-5 py-1 text-sm mb-3 text-white transition duration-200 hover:bg-primary">
                               <i className="fas fa-plus mr-2"></i>Add
-                        </button>
+                </button>
+                <div className="relative">
+                    <button
+                      className="rounded-full bg-secondary font-body px-5 py-1 text-sm mb-3 text-white transition duration-200 hover:bg-primary cursor-pointer"
+                    >
+                      <i className="fas fa-file-import mr-1"></i> Import
+                    </button>
+                    <input
+                      type="file"
+                      accept=".xlsx"
+                      className="absolute inset-0 opacity-0 cursor-pointer"
+                      onChange={handleFileUpload}
+                    />
+                  </div>
+
+
+                  <CSVLink data={data} 
+                    type="button" 
+                      className="rounded-full bg-secondary font-body px-5 py-1 text-sm mb-3 text-white transition duration-200 hover:bg-primary" >  Export  <FileUploadIcon />
+                  </CSVLink>
                     </div>
                     {/* DataGrid */}
                     <div style={{ marginLeft: '-11px', marginRight: '-11px' }}>
