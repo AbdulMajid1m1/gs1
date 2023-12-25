6da9f0064f2edea665e6d0a720b6aab05f9a95fd
Eissanoor <86971746+Eissanoor@users.noreply.github.com>
2023-12-25 23:45:47 +0500

    cr numbers

--- Frontend/src/Pages/AdminPages/MasterData/crnumber/crnumber.jsx
+++ Frontend/src/Pages/AdminPages/MasterData/crnumber/crnumber.jsx
@@ -15,6 +15,9 @@ import Swal from 'sweetalert2';
 import {toast} from 'react-toastify';
 import Addcrnumber from './addcrnumber';
 import Updatecrnumber from './updatecrnumber';
+import * as XLSX from 'xlsx';
+import { CSVLink } from "react-csv";
+import FileUploadIcon from '@mui/icons-material/FileUpload';
 const crnumber = () => {
 
     const [isLoading, setIsLoading] = useState(true);
@@ -156,7 +159,51 @@ const handleDelete = async (row) => {
         }
     
       }
+const handleFileUpload = (e) => {
+        const file = e.target.files[0];
+        if (file) {
+            console.log(file.type);
+            const reader = new FileReader();
+            reader.onload = (e) => {
+                const data = new Uint8Array(e.target.result);
+                const workbook = XLSX.read(data, { type: 'array' });
+                const sheetName = workbook.SheetNames[0]; // Assuming you have data in the first sheet
+                const sheet = workbook.Sheets[sheetName];
+                const json = XLSX.utils.sheet_to_json(sheet);
+                json.forEach((item) => {
+                    newRequest.post(`/crs`, {
+                        cr: item.cr, // Adjust property names as needed
+                      activity: item.activity,
+                     
+                      status: 1,
+                     
+                     
+                    })
+                        .then((res) => {
+                            console.log('Add', res.data);
+                            
+                            Swal.fire(
+                                'Add!',
+                                `Cr number has been created`,
+                                'success'
+                            )
+                           refreshcitiesData()
+                        })
+                        .catch((err) => {
+                            console.log(err);
+                            Swal.fire(
+                                'Error!',
+                                `Some Cr number already exist`,
+                                'error'
+                            )
+                            // Handle errors
+                        });
+                });
+            };
+            reader.readAsArrayBuffer(file);
 
+        }
+    };
   return (
     <div>
         <div className="p-0 h-full sm:ml-72">
@@ -175,7 +222,27 @@ const handleDelete = async (row) => {
                           onClick={handleShowCreatePopup}
                             className="rounded-full bg-secondary font-body px-5 py-1 text-sm mb-3 text-white transition duration-200 hover:bg-primary">
                               <i className="fas fa-plus mr-2"></i>Add
-                        </button>
+                </button>
+                 <div className="relative">
+                    <button
+                      className="rounded-full bg-secondary font-body px-5 py-1 text-sm mb-3 text-white transition duration-200 hover:bg-primary cursor-pointer"
+                    >
+                      <i className="fas fa-file-import mr-1"></i> Import
+                    </button>
+                    <input
+                      type="file"
+                      accept=".xlsx"
+                      className="absolute inset-0 opacity-0 cursor-pointer"
+                      onChange={handleFileUpload}
+                    />
+                  </div>
+
+
+                <CSVLink data={data}
+                  
+                    type="button" 
+                      className="rounded-full bg-secondary font-body px-5 py-1 text-sm mb-3 text-white transition duration-200 hover:bg-primary" >  Export  <FileUploadIcon />
+                  </CSVLink>
                     </div>
                     {/* DataGrid */}
                     <div style={{ marginLeft: '-11px', marginRight: '-11px' }}>
