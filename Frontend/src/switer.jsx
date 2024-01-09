6ddccfc4c8d7808c7e1be44ac258038e78649e76
zakirya592 <muhammadzakirya592@gmail.com>
2024-01-10 00:14:55 +0500

    uppdata

--- Frontend/src/switer.jsx
+++ Frontend/src/switer.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState} from 'react';
 import { useTranslation } from 'react-i18next';
 import engflage from "./Images/Flage.png"
 
@@ -13,7 +13,7 @@ const LanguageSwitcher = () =>
 
     const changeLanguage = (language) => {
         i18n.changeLanguage(language);
-    };
+    }; 
     return (
         <div>
             <select
@@ -23,15 +23,12 @@ const LanguageSwitcher = () =>
                 onChange={(e) => changeLanguage(e.target.value)}
             >
 
-                <option value="en" >
-                    <div className="flex items-center bg-red" >
-                        English <img src={engflage} alt="English" className="mr-2" />
-                    </div>
+                <option value="en" data-imagesrc="./Images/Flage.png" >
+                        English
                 </option>
                 <option value="ar">العربية</option>
             </select>
-
-            <img src={engflage} alt="English" className="mr-2" />
+             
         </div>
 
     );
