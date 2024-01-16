acd95944f06f32b2c4857f576f9524b10eb8ca16
fawadshah95 <fawad.shah@enviovista.com>
2024-01-16 20:43:10 +0500

    Commit

--- Frontend/src/Pages/MemberPages/GTIN/GTIN.jsx
+++ Frontend/src/Pages/MemberPages/GTIN/GTIN.jsx
@@ -56,31 +56,22 @@ const Gtin = () => {
 
 
    
-  // useEffect(() => {
     const fetchGtinProducts = async () => {
        try {
          const response = await newRequest.get(`/gtinProducts/subcriptionsProducts?status=active&user_id=${memberData?.id}&isDeleted=false`);
-         console.log(response.data);
-        //  setExpiryDate(response?.data?.gtinSubscriptions[0]?.expiry_date);
- 
+        //  console.log(response.data);
+     
         //  setGtinSubscriptions(response?.data?.gtinSubscriptions);
          setTotalCategory(response?.data?.gtinSubscriptions[0]?.gtin_product?.member_category_description);
-        console.log(response?.data?.gtinSubscriptions[0]?.gtin_product?.member_category_description);
-        console.log(totalCategory)
-        //  setTotalRange(response?.data?.gtinSubscriptions[0]?.gtin_product?.total_no_of_barcodes);
-        //  setGtinBarcodeIssued(response?.data?.gtinSubscriptions[0]?.gtin_subscription_counter);
-        //  setGtinBarcodeRemaining(response?.data?.gtinSubscriptions[0]?.gtin_subscription_limit);
-         
- 
-        //  setOtherProductSubscriptions(response?.data?.otherProductSubscriptions);
- 
+         console.log(response?.data?.gtinSubscriptions[0]?.gtin_product?.member_category_description);
+         console.log(totalCategory)
+    
  
       
        } catch (err) {
          console.log(err);
        }
      }
-     //  }, []) 
      
      
      
