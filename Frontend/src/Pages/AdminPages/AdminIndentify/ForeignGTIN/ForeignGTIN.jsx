48e8cbd3497071fa87fc7accd1bf455a6d3432cf
zakirya592 <muhammadzakirya592@gmail.com>
2024-01-17 23:49:48 +0500

    updtaa

--- Frontend/src/Pages/AdminPages/AdminIndentify/ForeignGTIN/ForeignGTIN.jsx
+++ Frontend/src/Pages/AdminPages/AdminIndentify/ForeignGTIN/ForeignGTIN.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { useTranslation } from 'react-i18next';
 
 const ForeignGTIN = () => {
+  const { t, i18n } = useTranslation();
   return (
     <div>
-      <div className="p-3 h-full sm:ml-72">
+      <div className={`p-0 h-full bg-slate-100 ${i18n.language === 'ar' ? 'sm:mr-72' : 'sm:ml-72'}`}>
         Foreign GTIN
       </div>
     </div>
