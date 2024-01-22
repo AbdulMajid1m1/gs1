7fee154dcf709896556a6f69bc65afb19852b67f
unknown <abdulmajid1m2@gmail.com>
2024-01-22 16:43:17 +0500

    d

--- Backend/routes/subRoutes/migrationRoutes.js
+++ Backend/routes/subRoutes/migrationRoutes.js
@@ -1,11 +1,16 @@
 
 import express from 'express';
-import { getMembershipHistory, searchMembers } from '../../controllers/migrationController.js';
+import { getMembershipHistory, migrateUser, searchMembers } from '../../controllers/migrationController.js';
 
 const router = express.Router();
 
 router.get('/user/search', searchMembers);
+
 router.get('/membershipHistory', getMembershipHistory);
+
+router.post("/migrateUser", migrateUser)
+
+
 
 
 export default router;
