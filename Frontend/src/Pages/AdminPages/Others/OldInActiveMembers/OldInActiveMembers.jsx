0bf26aac1d0eb44ba6db0427034b4e08e4f2c235
unknown <abdulmajid1m2@gmail.com>
2024-01-23 16:25:57 +0500

    added m codess

--- Frontend/src/Pages/AdminPages/Others/OldInActiveMembers/OldInActiveMembers.jsx
+++ Frontend/src/Pages/AdminPages/Others/OldInActiveMembers/OldInActiveMembers.jsx
@@ -24,6 +24,7 @@ const OldInActiveMembers = () => {
   const [open, setOpen] = useState(false);
   const [oldMemberList, setOldMemberList] = useState([]);
   const abortControllerRef = React.useRef(null);
+  const [amount, setAmount] = useState(null);
 
   const handleGPCAutoCompleteChange = (event, value) => {
     setSelectedOldMember(value);
@@ -38,7 +39,7 @@ const OldInActiveMembers = () => {
   }
 
   const [details, setDetails] = useState([])
-  
+
   // Use debounce to wrap the handleAutoCompleteInputChange function
   const debouncedHandleAutoCompleteInputChange = debounce(async (event, newInputValue, reason) => {
     console.log(reason);
@@ -51,7 +52,7 @@ const OldInActiveMembers = () => {
     if (reason === 'option') {
       return; // Do not perform search if the option is selected
     }
-    
+
     if (!newInputValue || newInputValue.trim() === '') {
       // perform operation when input is cleared
       setOldMemberList([]);
@@ -73,7 +74,7 @@ const OldInActiveMembers = () => {
         signal: abortControllerRef.current.signal
       });
       console.log(res);
-      
+
       const crs = res?.data?.map(item => {
         return {
           IntID: item.IntID,
@@ -89,12 +90,12 @@ const OldInActiveMembers = () => {
 
       setOldMemberList(crs);
       setDetails(res?.data[0]);
-   
+
       setOpen(true);
       setAutocompleteLoading(false);
-      
+
       // fetchData();
-      
+
     } catch (error) {
       console.error(error);
       setOldMemberList([]); // Clear the data list if an error occurs
@@ -102,8 +103,8 @@ const OldInActiveMembers = () => {
       setAutocompleteLoading(false);
     }
   }, 400);
-  
-  
+
+
   // const [allSearchMemberDetails, setAllSearchMemberDetails] = useState('')
 
   const fetchData = async (value) => {
@@ -115,8 +116,9 @@ const OldInActiveMembers = () => {
       // const response = await newRequest.get(`/migration/membershipHistory?MemberID=521`);
       setData(response?.data?.MembershipHistory || []);
       setYearsToPay(response?.data?.YearsToPay || [])
+      setAmount(response?.data?.yearlyAmount)
       console.log(response.data);
-     
+
       setIsLoading(false);
     } catch (err) {
       console.log(err);
@@ -136,7 +138,7 @@ const OldInActiveMembers = () => {
     // console.log(selectedOldMember);
     if (!selectedOldMember) {
       toast.info('Please select a member first');
-      setMigrateButtonLoader(false); 
+      setMigrateButtonLoader(false);
       return;
     }
 
@@ -154,13 +156,13 @@ const OldInActiveMembers = () => {
     }
   }
 
-  
+
   return (
     <div>
       <div className={`p-0 h-full ${i18n.language === 'ar' ? 'sm:mr-72' : 'sm:ml-72'}`}>
         <div>
           <AdminDashboardRightHeader title={t('Old InActive Members')} />
-        </div>   
+        </div>
 
 
         <div className='flex justify-center items-center'>
@@ -168,128 +170,128 @@ const OldInActiveMembers = () => {
             <div className="h-auto w-full p-2 bg-white shadow-xl rounded-md">
 
 
-        <div className="px-3">
-            <Autocomplete
-              id="companyName"
-              required
-              options={oldMemberList}
-              // IntID: item.IntID,
-              // MemberID: item.MemberID,
-              // MemberNameE: item.MemberNameE,
-              // MemberNameA: item.MemberNameA,
-              // Email: item.Email,
-              getOptionLabel={(option) => (option && option.IntID) ? `${option?.Phone1} - ${option?.MemberNameE} - ${option?.MemberNameA} - ${option?.Email} - ${option?.GLNID}` : ''}
-              onChange={handleGPCAutoCompleteChange}
-              value={selectedOldMember}
-              onInputChange={(event, newInputValue, params) => debouncedHandleAutoCompleteInputChange(event, newInputValue, params)}
-              loading={autocompleteLoading}
-              sx={{ marginTop: '10px' }}
-              open={open}
-              onOpen={() => {
-                // setOpen(true);
-              }}
-              onClose={() => {
-                setOpen(false);
-              }}
-              renderOption={(props, option) => (
-                <li key={option.IntID} {...props}>
-                  {option ? `${option.IntID} - ${option.Phone1} - ${option.MemberNameE} - ${option.MemberNameA} - ${option.Email} - ${option?.GLNID}` : 'No options'}
-                </li>
-              )}
-
-
-              renderInput={(params) => (
-                <TextField
-                  // required
-                  error={isSubmitClicked && !selectedOldMember?.cr}
-                  helperText={isSubmitClicked && !selectedOldMember?.cr ? "Products is required" : ""}
-                  {...params}
-                  label={`${t('Search Members')}`}
-                  InputProps={{
-                    ...params.InputProps,
-                    endAdornment: (
-                      <React.Fragment>
-                        {autocompleteLoading ? <CircularProgress color="inherit" size={20} /> : null}
-                        {params.InputProps.endAdornment}
-                      </React.Fragment>
-                    ),
+              <div className="px-3">
+                <Autocomplete
+                  id="companyName"
+                  required
+                  options={oldMemberList}
+                  // IntID: item.IntID,
+                  // MemberID: item.MemberID,
+                  // MemberNameE: item.MemberNameE,
+                  // MemberNameA: item.MemberNameA,
+                  // Email: item.Email,
+                  getOptionLabel={(option) => (option && option.IntID) ? `${option?.Phone1} - ${option?.MemberNameE} - ${option?.MemberNameA} - ${option?.Email} - ${option?.GLNID}` : ''}
+                  onChange={handleGPCAutoCompleteChange}
+                  value={selectedOldMember}
+                  onInputChange={(event, newInputValue, params) => debouncedHandleAutoCompleteInputChange(event, newInputValue, params)}
+                  loading={autocompleteLoading}
+                  sx={{ marginTop: '10px' }}
+                  open={open}
+                  onOpen={() => {
+                    // setOpen(true);
                   }}
-                  sx={{
-                    '& label.Mui-focused': {
-                      color: '#00006A',
-                    },
-                    '& .MuiInput-underline:after': {
-                      borderBottomColor: '#00006A',
-                    },
-                    '& .MuiOutlinedInput-root': {
-                      '&:hover fieldset': {
-                        borderColor: '#000000',
-                      },
-                      '&.Mui-focused fieldset': {
-                        borderColor: '#000000',
-                      },
-                    },
+                  onClose={() => {
+                    setOpen(false);
                   }}
-                />
-              )}
+                  renderOption={(props, option) => (
+                    <li key={option.IntID} {...props}>
+                      {option ? `${option.IntID} - ${option.Phone1} - ${option.MemberNameE} - ${option.MemberNameA} - ${option.Email} - ${option?.GLNID}` : 'No options'}
+                    </li>
+                  )}
 
-            />
 
-          </div>
-
-          
-          <div  className={`flex justify-center sm:justify-start items-center flex-wrap gap-2 py-3 px-3 mt-4 ${i18n.language === 'ar' ? 'flex-row-reverse justify-start' : 'flex-row justify-start'}`}>
-            <button
-              className="rounded-full bg-[#1E3B8B] font-body px-5 py-1 text-sm mb-3 text-white transition duration-200 hover:bg-primary"
-            >
-              Renewal upto The Year {new Date().getFullYear()}
-            </button>
-
-            <button
-              className="rounded-full bg-[#1E3B8B] font-body px-5 py-1 text-sm mb-3 text-white transition duration-200 hover:bg-primary">
-               Number Of Year: {YearsToPay}
-            </button>
-
-            <button
-              className="rounded-full bg-[#1E3B8B] font-body px-5 py-1 text-sm mb-3 text-white transition duration-200 hover:bg-primary">
-              Amount
-            </button>
-
-          </div>
-
-          <div style={{ marginLeft: '-11px', marginRight: '-11px' }}>
-
-            <DataTable data={data} title={t('Old InActive Members')} columnsName={oldInActiveMemberColumn}
-              loading={isLoading}
-              secondaryColor="gray"
-              // handleRowClickInParent={handleRowClickInParent}
-              uniqueId="customerListId"
-              actionColumnVisibility={false}
-              showToolbarSlot={false}
-              checkboxSelection={'disabled'}
-
-              dropDownOptions={[
-                {
-                  label: `${t('View')}`,
-                  icon: (
-                    <VisibilityIcon
-                      fontSize="small"
-                      color="action"
-                      style={{ color: "rgb(37 99 235)" }}
+                  renderInput={(params) => (
+                    <TextField
+                      // required
+                      error={isSubmitClicked && !selectedOldMember?.cr}
+                      helperText={isSubmitClicked && !selectedOldMember?.cr ? "Products is required" : ""}
+                      {...params}
+                      label={`${t('Search Members')}`}
+                      InputProps={{
+                        ...params.InputProps,
+                        endAdornment: (
+                          <React.Fragment>
+                            {autocompleteLoading ? <CircularProgress color="inherit" size={20} /> : null}
+                            {params.InputProps.endAdornment}
+                          </React.Fragment>
+                        ),
+                      }}
+                      sx={{
+                        '& label.Mui-focused': {
+                          color: '#00006A',
+                        },
+                        '& .MuiInput-underline:after': {
+                          borderBottomColor: '#00006A',
+                        },
+                        '& .MuiOutlinedInput-root': {
+                          '&:hover fieldset': {
+                            borderColor: '#000000',
+                          },
+                          '&.Mui-focused fieldset': {
+                            borderColor: '#000000',
+                          },
+                        },
+                      }}
                     />
-                  ),
-                  // action: handleView,
-                },
-                {
-                  label: `${t('Delete')}`,
-                  icon: <DeleteIcon fontSize="small" style={{ color: '#FF0032' }} />
-                  ,
-                  // action: handleDelete,
-                }
+                  )}
 
-              ]}
+                />
 
-            />
+              </div>
+
+
+              <div className={`flex justify-center sm:justify-start items-center flex-wrap gap-2 py-3 px-3 mt-4 ${i18n.language === 'ar' ? 'flex-row-reverse justify-start' : 'flex-row justify-start'}`}>
+                <button
+                  className="rounded-full bg-[#1E3B8B] font-body px-5 py-1 text-sm mb-3 text-white transition duration-200 hover:bg-primary"
+                >
+                  Renewal upto The Year {new Date().getFullYear()}
+                </button>
+
+                <button
+                  className="rounded-full bg-[#1E3B8B] font-body px-5 py-1 text-sm mb-3 text-white transition duration-200 hover:bg-primary">
+                  Number Of Year: {YearsToPay}
+                </button>
+
+                <button
+                  className="rounded-full bg-[#1E3B8B] font-body px-5 py-1 text-sm mb-3 text-white transition duration-200 hover:bg-primary">
+                  Amount: {amount}
+                </button>
+
+              </div>
+
+              <div style={{ marginLeft: '-11px', marginRight: '-11px' }}>
+
+                <DataTable data={data} title={t('Old InActive Members')} columnsName={oldInActiveMemberColumn}
+                  loading={isLoading}
+                  secondaryColor="gray"
+                  // handleRowClickInParent={handleRowClickInParent}
+                  uniqueId="customerListId"
+                  actionColumnVisibility={false}
+                  showToolbarSlot={false}
+                  checkboxSelection={'disabled'}
+
+                  dropDownOptions={[
+                    {
+                      label: `${t('View')}`,
+                      icon: (
+                        <VisibilityIcon
+                          fontSize="small"
+                          color="action"
+                          style={{ color: "rgb(37 99 235)" }}
+                        />
+                      ),
+                      // action: handleView,
+                    },
+                    {
+                      label: `${t('Delete')}`,
+                      icon: <DeleteIcon fontSize="small" style={{ color: '#FF0032' }} />
+                      ,
+                      // action: handleDelete,
+                    }
+
+                  ]}
+
+                />
 
 
                 <div className='w-full flex justify-start px-6 pt-2 py-6 gap-2'>
@@ -299,26 +301,26 @@ const OldInActiveMembers = () => {
                     >
                       Migrate & Generate Invoice
                     </button> */}
-                    <Button
-                      variant="contained"
-                      style={{ backgroundColor: '#021F69', color: '#ffffff' }}
-                      type="button"
-                      onClick={handleMigrateAndGenerateMember}
-                      disabled={migrateButtonLoader}
-                      className="ml-2"
-                      endIcon={migrateButtonLoader ? <CircularProgress size={24} color="inherit" /> : null}
-                      >
-                      Migrate & Generate Invoice
-                    </Button>
+                  <Button
+                    variant="contained"
+                    style={{ backgroundColor: '#021F69', color: '#ffffff' }}
+                    type="button"
+                    onClick={handleMigrateAndGenerateMember}
+                    disabled={migrateButtonLoader}
+                    className="ml-2"
+                    endIcon={migrateButtonLoader ? <CircularProgress size={24} color="inherit" /> : null}
+                  >
+                    Migrate & Generate Invoice
+                  </Button>
                 </div>
-          </div>
+              </div>
 
 
             </div>
           </div>
         </div>
 
- 
+
       </div>
     </div>
   )
