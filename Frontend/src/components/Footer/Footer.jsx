c58c4f3b376df56f4421587f8e6ac7bb95c5f07b
fawadshah95 <fawad.shah@enviovista.com>
2024-02-22 15:43:26 +0500

    Commit

--- Frontend/src/components/Footer/Footer.jsx
+++ Frontend/src/components/Footer/Footer.jsx
@@ -13,7 +13,7 @@ const Footer = () => {
   const { t } = useTranslation();
   return (
     <div>
-        <div className='py-4 gap-2 sm:px-16 px-8 sm:h-[351px] h-auto w-full bg-secondary relative'>
+        <div className='py-4 gap-2 sm:px-16 px-8 3xl::h-[351px] 2xl:h-[351px] xl:h-[351px] lg:h-[351px] h-auto w-full bg-secondary relative'>
          
          <div className='w-full grid 2xl:grid-cols-3 lg:grid-cols-3 grid-cols-1'>
           <div className={`h-auto w-full flex flex-col gap-8 relative ${i18n.language === 'ar' ? 'order-last' : 'order-first'}`}>
@@ -53,42 +53,55 @@ const Footer = () => {
 
          </div>
 
-        <div className={`flex ${i18n.language === 'ar' ? 'lg:flex-row-reverse' : 'lg:flex-row'} flex-col justify-center items-center sm:mt-16 mt-6 sm:flex-row sm:justify-between sm:items-center `}>
-          <h2 className='text-white mb-2 sm:mb-0'> {t('Copyright @GS1 Saudi Arabia - 2023 All rights reserved.')}</h2>
-             <div className='flex flex-wrap gap-4'>
-               <img
-                 src={whatsApp}
-                 alt=""
-                 className='h-12 w-12 p-2 object-contain'
-               />
-               <img
-                 src={linkedIn}
-                 alt=""
-                 className='h-12 w-12 p-2 object-contain'
-               />
-               <img
-                 src={facebook}
-                 alt=""
-                 className='h-12 w-12 p-2 object-contain'
-               />
-               <img
-                 src={twitter}
-                 alt=""
-                 className='h-12 w-12 p-2 object-contain'
-               />
+
+          <div className={`flex ${i18n.language === 'ar' ? 'lg:flex-row-reverse' : 'lg:flex-row'} flex-col justify-center items-center sm:mt-16 mt-6 sm:flex-row sm:justify-between sm:items-center `}>
+            <h2 className='text-white font-normal sm:font-semibold mb-2 sm:mb-0'> {t('Copyright @GS1 Saudi Arabia - 2024 All rights reserved.')}</h2>
+            <div className='flex flex-wrap gap-4'>
+              <a href="https://api.whatsapp.com/send?text=YourShareTextHere&phone=+966112182428" target="_blank" rel="noopener noreferrer">
                 <img
-                 src={instagram}
-                 alt=""
-                 className='h-12 w-12 p-2 object-contain'
-               />
+                  src={whatsApp}
+                  alt=""
+                  className='h-12 w-12 p-2 object-contain hover:scale-110 transition-transform duration-300 ease-in-out'
+                />
+              </a>
+              <a href="https://www.linkedin.com/in/gs1-saudi-ksa-389876263/" target="_blank" rel="noopener noreferrer">
                 <img
-                 src={youtube}
-                 alt=""
-                 className='h-12 w-12 p-2 object-contain'
-               />
-             </div>
-           </div>
+                  src={linkedIn}
+                  alt=""
+                  className='h-12 w-12 p-2 object-contain hover:scale-110 transition-transform duration-300 ease-in-out'
+                />
+              </a>
+              <a href="https://www.linkedin.com/in/gs1-saudi-ksa-389876263/" target="_blank" rel="noopener noreferrer">
+                <img
+                  src={facebook}
+                  alt=""
+                  className='h-12 w-12 p-2 object-contain hover:scale-110 transition-transform duration-300 ease-in-out'
+                />
+              </a>
+              <a href="https://twitter.com/gs1Saudi" target="_blank" rel="noopener noreferrer">
+                <img
+                  src={twitter}
+                  alt=""
+                  className='h-12 w-12 p-2 object-contain hover:scale-110 transition-transform duration-300 ease-in-out'
+                />
+              </a>
+              <a href="https://www.instagram.com/gs1saudi/" target="_blank" rel="noopener noreferrer">
+                <img
+                  src={instagram}
+                  alt=""
+                  className='h-12 w-12 p-2 object-contain hover:scale-110 transition-transform duration-300 ease-in-out'
+                />
+              </a>
+              <a href="https://www.youtube.com/@gs1global/videos" target="_blank" rel="noopener noreferrer">
+                <img
+                  src={youtube}
+                  alt=""
+                  className='h-12 w-12 p-2 object-contain hover:scale-110 transition-transform duration-300 ease-in-out'
+                />
+              </a>
+            </div>
           </div>
+        </div>
     </div>
   )
 }
