f28d3a8680bd926a1cef31849bd05762ecc10877
unknown <abdulmajid1m2@gmail.com>
2024-02-27 16:36:58 +0500

    d

--- Frontend/src/ProtectedRoute.jsx
+++ Frontend/src/ProtectedRoute.jsx
@@ -1,34 +1,52 @@
-import React, { useContext, useRef } from 'react';
-import { Navigate } from 'react-router-dom';
+import React, { useContext, useEffect, useState } from 'react';
+import { Navigate, Route } from 'react-router-dom';
 import { AuthContext } from './Contexts/AuthContext';
 import { toast } from 'react-toastify';
 
 const ProtectedRoute = ({ children, requiredPermission }) => {
     const { permissions } = useContext(AuthContext);
+    const [toastShown, setToastShown] = useState(false);
+
     const adminData = sessionStorage.getItem('adminData');
-    console.log(adminData);
-    // Check if admin data exists
+    console.log('Admin Data:', adminData);
+
+    useEffect(() => {
+        const loginToastShown = localStorage.getItem('loginToastShown');
+
+        // Check if admin data exists
+        if (!adminData && !loginToastShown) {
+            console.log('Showing login toast');
+            toast.error('Please log in to access this page');
+            localStorage.setItem('loginToastShown', 'true');
+            setToastShown(true);
+        }
+    }, [adminData]);
+
     if (!adminData) {
-        toast.error('Please log in to access this page');
-        return <Navigate to="/admin/admin-login" />;
+        console.log('User not logged in, navigating to login page');
+        return <Navigate to="/admin-login" />;
     }
 
     const parsedAdminData = JSON.parse(adminData);
 
     // Checking for super admin
     if (parsedAdminData.is_super_admin === 1) {
+        console.log('Super admin, allowing access');
         return children;
     }
 
     if (!permissions.includes(requiredPermission)) {
         const toastShownKey = `toastShown_${requiredPermission}`;
         if (!sessionStorage.getItem(toastShownKey)) {
+            console.log(`Showing permission toast for ${requiredPermission}`);
             toast.error(`You don't have permission ${requiredPermission} to access this page`);
             sessionStorage.setItem(toastShownKey, 'true');
         }
+        console.log('Navigating to admin dashboard');
         return <Navigate to="/admin/dashboard" />;
     }
 
+    console.log('User has required permission, allowing access');
     return children;
 };
 
