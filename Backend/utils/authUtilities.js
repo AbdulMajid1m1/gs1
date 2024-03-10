b3fa9798342f29ffb1c6ca27240fab3b2ce108de
gilfr3d <gil@bsitco.com>
2024-03-10 13:59:57 +0300

    d

--- Backend/utils/authUtilities.js
+++ Backend/utils/authUtilities.js
@@ -9,17 +9,17 @@ export const userDataProperties = user => ({
     updatedAt: user.updatedAt
 });
 // for https:
-export const cookieOptions = () => ({
-    httpOnly: true,
-    // secure: true,
-    secure: false,// False to Allow both HTTP and HTTPS
-    sameSite: 'Lax', // Lax to allow CSRF protection by default and strict to disallow CSRF protection and None to disable CSRF protection
-});
+// export const cookieOptions = () => ({
+//     httpOnly: true,
+//     // secure: true,
+//     secure: false,// False to Allow both HTTP and HTTPS
+//     sameSite: 'Lax', // Lax to allow CSRF protection by default and strict to disallow CSRF protection and None to disable CSRF protection
+// });
 
 
 // for http 
-// export const cookieOptions = () => ({
-//     httpOnly: true,
-//     secure: false, // Allow both HTTP and HTTPS
-//     sameSite: 'Lax', // Lax to allow CSRF protection by default and strict to disallow CSRF protection and None to disable CSRF protection
-// });
+export const cookieOptions = () => ({
+    httpOnly: true,
+    secure: false, // Allow both HTTP and HTTPS
+    sameSite: 'Lax', // Lax to allow CSRF protection by default and strict to disallow CSRF protection and None to disable CSRF protection
+});
