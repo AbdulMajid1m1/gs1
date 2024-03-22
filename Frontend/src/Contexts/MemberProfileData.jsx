08b59bc14240661cbf34278556e0fa07d1379989
fawadshah95 <fawad.shah@enviovista.com>
2024-03-22 15:43:54 +0500

    Commit

--- Frontend/src/Contexts/MemberProfileData.jsx
+++ Frontend/src/Contexts/MemberProfileData.jsx
@@ -1,15 +1,16 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
 import newRequest from '../utils/userRequest';
 
 export const MemberProfileDataContext = createContext();
 
 const MemberProfileData = ({ children }) => {
     const [memberProfileDetails, setMemberProfileDetails] = useState([]);
-    
-    const fetchAllUserData = async (userId) => {
-        // console.log("fetching user data", userId)
+    const memberDataString = sessionStorage.getItem('memberData');
+    const memberData = JSON.parse(memberDataString);
+   
+    const fetchAllUserData = async () => {
         try {
-          const response = await newRequest.get(`/users?id=${userId}`);
+          const response = await newRequest.get(`/users?id=${memberData?.id}`);
         //   console.log(response.data[0]);
           setMemberProfileDetails(response?.data[0] || []);
             
@@ -19,6 +20,11 @@ const MemberProfileData = ({ children }) => {
         //   console.log(err);
         }
     };
+
+   
+    useEffect(() => {
+        fetchAllUserData();
+    }, [])
 
     return (
         <MemberProfileDataContext.Provider value={{
